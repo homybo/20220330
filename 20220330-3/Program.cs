d55243049d5d78924e9a37293a7203906c344666
user <user@A401-PC22>
2022-03-30 16:29:18 +0800

    加入專案檔案。

---
+++ 20220330-3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220330_3
+{
+    class Program
+    {
+        private static bool Prime(int n)
+        {
+            bool is_prime = true;
+            for (int i = 2; i < n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    is_prime = false;
+                }
+            }
+            return is_prime;
+            
+        }
+        static void Main(string[] args)
+        {
+            bool is_prime = false;
+            for (int i = 100; i < 501; i++)
+            {
+                is_prime = Prime(i);
+                if (is_prime == true)
+                {
+                    Console.WriteLine(i + "是質數");
+                }
+
+            }
+            Console.ReadLine();
+            /*1-500*/
+        }
+    }
+}
